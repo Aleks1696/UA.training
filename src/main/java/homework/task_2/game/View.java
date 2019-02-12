50d73457b612c2691ef01f568acc3523d3468c6c
Aleks1696 <aleks.borodavka@ukr.net>
2019-02-12 13:40:43 +0200

    View file

--- src/main/java/homework/task_2/game/View.java
+++ src/main/java/homework/task_2/game/View.java
@@ -2,18 +2,15 @@ package homework.task_2.game;
 
 public class View {
 
-    public static final String HELLO_MESSAGE = "Welcome to 'Greater or Less' game";
-    public static final String RANGE_MESSAGE = "Enter numbers range (example: 20-68) or 'default'";
-    public static final String GAME_STARTED_MESSAGE = "Ok, try to guess now. (Range: %d - %d)";
-    public static final String WRONG_NUMBER_FORMAT_MESSAGE = "Wrong number format.";
-    public static final String GREATER_MESSAGE = "Greater";
-    public static final String LESS_MESSAGE = "Less";
-    public static final String ALREADY_ENTERED_NUMBERS_MESSAGE = "You have already tried: ";
-    public static final String CONGRATULATION_MESSAGE = "Congratulation!!! Attempts: %d ";
+    public static final String HELLO_MESSAGE = "Welcome to 'Greater or Less' game!";
+    public static final String GAME_STARTED_MESSAGE = "Ok, let's start. Try to guess now. (Bounds: [%d - %d])";
+    public static final String TRY_AGAIN_MESSAGE = "Try again (Bounds: [%d - %d])";
+    public static final String WRONG_NUMBER_FORMAT_MESSAGE = "Wrong number format";
+    public static final String NUMBER_OUT_OF_BOUNDS_MESSAGE = "Number is out of bounds";
+    public static final String CONGRATULATION_MESSAGE = "Congratulations! Attempts: %d " +
+            "Entered numbers: %s";
 
-    public void print(String ... messages){
-        for (String s : messages) {
-            System.out.println(s);
-        }
+    public void print(String message) {
+        System.out.println(message);
     }
 }
