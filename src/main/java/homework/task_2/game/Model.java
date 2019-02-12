eda612bbbcc1807e29d2bea1459c458586d5cdef
Aleks1696 <aleks.borodavka@ukr.net>
2019-02-12 13:44:22 +0200

    Model file

--- src/main/java/homework/task_2/game/Model.java
+++ src/main/java/homework/task_2/game/Model.java
@@ -4,36 +4,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Model {
-
-    private int DEFAULT_LOWER_BOUND = 0;
-    private int DEFAULT_UPPER_BOUND = 100;
+    private int generatedNumber;
     private int lowerBound;
     private int upperBound;
 
-    private int number;
+    private List<Integer> inputHistoryList = new ArrayList<>();
 
-    private List<Integer> inputHistoryList = inputHistoryList = new ArrayList<>();
-
-    public int generateRandomNumber() {
-        DEFAULT_UPPER_BOUND -= DEFAULT_LOWER_BOUND;
-        number = (int) (Math.random() * ++DEFAULT_UPPER_BOUND) + DEFAULT_LOWER_BOUND;
-        return number;
-    }
-
-    public int generateRandomNumber(int lowerBound, int upperBound){
+    public void setBounds(int lowerBound, int upperBound) {
         this.lowerBound = lowerBound;
         this.upperBound = upperBound;
-        upperBound -= lowerBound;
-        number = (int) (Math.random() * ++upperBound) + lowerBound;
-        return number;
     }
 
-    public void writeInputToHistory(int inputNumber) {
-        inputHistoryList.add(inputNumber);
+    public void generateRandomNumber() {
+        generatedNumber = (int) (Math.random() * (upperBound - (lowerBound + 1)) + lowerBound + 1);
     }
 
-    public List<Integer> getInputHistoryList() {
-        return inputHistoryList;
+    public boolean checkNumber(int number) {
+        inputHistoryList.add(number);
+        if (number == generatedNumber) {
+            return false;
+        } else if (number > generatedNumber) {
+            upperBound = number;
+            return true;
+        } else {
+            lowerBound = number;
+            return true;
+        }
+    }
+
+    public int getGeneratedNumber() {
+        return generatedNumber;
     }
 
     public int getLowerBound() {
@@ -44,15 +44,7 @@ public class Model {
         return upperBound;
     }
 
-    public int getNumber() {
-        return number;
-    }
-
-    public int getDEFAULT_LOWER_BOUND() {
-        return DEFAULT_LOWER_BOUND;
-    }
-
-    public int getDEFAULT_UPPER_BOUND() {
-        return DEFAULT_UPPER_BOUND;
+    public List<Integer> getInputHistoryList() {
+        return inputHistoryList;
     }
 }
