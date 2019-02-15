7ac9a9879b8c9c527438920e0d70b3f0bc458867
Aleks1696 <aleks.borodavka@ukr.net>
2019-02-15 16:03:05 +0200

    constant messages for informing user with format examples

---
+++ src/main/java/homework/block_1_task_regex/view/Messages.java
@@ -0,0 +1,18 @@
+package homework.block_1_task_regex.view;
+
+public interface Messages {
+
+    String INFO_MESSAGE = "Create new note following next hints";
+
+    String ERROR_MESSAGE = "Wrong input format. Try again";
+
+    String SURNAME_NAME_MESSAGE = "Enter surname (example: 'Ivanova')";
+    String FIRST_NAME_MESSAGE = "Enter first name (example: 'Maria')";
+    String FAMILY_NAME_MESSAGE = "Enter family name (example: 'Dmitrievna')";
+    String NICK_NAME_MESSAGE = "Enter nick name (example: 'Masha82')";
+    String COMMENTARY_MESSAGE = "Enter commentary (example: 'This note is creating for business purposes ect.')";
+    String HOME_PHONE_NUMBER_MESSAGE = "Enter home phone number (example: '+XXX-XXX-XX-XX')";
+    String MOBILE_PHONE_NUMBER_MESSAGE = "Enter mobile phone number (example: '+XXX(XX)-XXX-XX-XX')";
+    String EMAIL_NAME_MESSAGE = "Enter email (example: 'maria@gmail.com')";
+    String SKYPE_NAME_MESSAGE = "Enter skype (example: 'masha_superStar')";
+}
