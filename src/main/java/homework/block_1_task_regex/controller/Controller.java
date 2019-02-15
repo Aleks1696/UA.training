c37406b3510e2b9ad0a2b62a8ed5d0d98ebea5f8
Aleks1696 <aleks.borodavka@ukr.net>
2019-02-15 16:07:13 +0200

    Main controller

---
+++ src/main/java/homework/block_1_task_regex/controller/Controller.java
@@ -0,0 +1,32 @@
+package homework.block_1_task_regex.controller;
+
+import homework.block_1_task_regex.model.Model;
+import homework.block_1_task_regex.view.Messages;
+import homework.block_1_task_regex.view.View;
+
+import java.util.Scanner;
+
+public class Controller {
+
+    private Model model;
+    private View view;
+
+    public Controller(Model model, View view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void processUser() {
+        view.print(Messages.INFO_MESSAGE);
+        Scanner scanner = new Scanner(System.in);
+        InputController inController = new InputController();
+        inController.getInput(new UtilityController(view, scanner));
+        model.createNote(inController.getSurname(), inController.getName(),
+                inController.getFamilyName(), inController.getNickName(),
+                inController.getCommentary(), inController.getHomePhoneNumber(),
+                inController.getMobilePhoneNumber(), inController.getEmail(),
+                inController.getSkype());
+        model.createNoteBook();
+        model.addCurrentNoteToNoteBook();
+    }
+}
