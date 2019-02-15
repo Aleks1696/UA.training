debbe1d2e618ca112a2d19c376a7aa3cbbf4a4a8
Aleks1696 <aleks.borodavka@ukr.net>
2019-02-15 16:05:26 +0200

    NoteBook entity for storing Notes

---
+++ src/main/java/homework/block_1_task_regex/model/entity/NoteBook.java
@@ -0,0 +1,21 @@
+package homework.block_1_task_regex.model.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteBook {
+
+    private List<Note> noteList = new ArrayList<>();
+
+    public List<Note> getNoteList() {
+        return noteList;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("NoteBook{");
+        sb.append("noteList=").append(noteList);
+        sb.append('}');
+        return sb.toString();
+    }
+}
