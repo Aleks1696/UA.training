017478c690b2d8e2ef5a1d6033e0213408598df5
Aleks1696 <aleks.borodavka@ukr.net>
2019-02-17 02:34:15 +0200

    UA and US regexes are added for name and nickName

--- src/main/java/homework/block_1_task_regex/controller/Regex.java
+++ src/main/java/homework/block_1_task_regex/controller/Regex.java
@@ -2,8 +2,10 @@ package homework.block_1_task_regex.controller;
 
 public interface Regex {
 
-    String NAME_NAME_REGEX = "((^[A-Z]{1})[a-z]{0,30}-?)(([A-Z]{1})[a-z]{0,30})?";
-    String NICK_NAME_REGEX = "(^[A-Za-z]{1,10}).{0,10}";
+    String US_NAME_REGEX = "^[A-Z]{1}[a-z]{0,30}(-[A-Z]{1}[a-z]{0,30})?";
+    String UA_NAME_REGEX = "^[А-ЩЮЯШЇЄҐ]{1}[а-щяьюяіїєґ']{0,30}(-[А-ЩЮЯШЇЄҐ]{1}[а-щяьюяіїєґ']{0,30})?";
+    String US_NICK_NAME_REGEX = "^[A-Za-z]{1,10}.{0,10}";
+    String UA_NICK_NAME_REGEX = "^[А-Ща-яьюЮяЯіІїЇЄєҐґ']{1,10}.{0,10}";
     String COMMENTARY_REGEX = ".+";
     String HOME_PHONE_NUMBER_REGEX = "^\\+\\d{3}-\\d{3}-\\d{2}-\\d{2}";
     String MOBILE_PHONE_NUMBER_REGEX = "^\\+\\d{3}\\(\\d{2}\\)-\\d{3}-\\d{2}-\\d{2}";
