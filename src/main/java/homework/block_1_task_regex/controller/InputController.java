4ffff61dd783fd237ddf3590d4bbec6d44562410
Aleks1696 <aleks.borodavka@ukr.net>
2019-02-17 02:36:04 +0200

    Additional checking was added to "getInput" method was added to define version either UA or US

--- src/main/java/homework/block_1_task_regex/controller/InputController.java
+++ src/main/java/homework/block_1_task_regex/controller/InputController.java
@@ -1,6 +1,9 @@
 package homework.block_1_task_regex.controller;
 
-import homework.block_1_task_regex.view.Messages;
+import homework.block_1_task_regex.view.View;
+
+import static homework.block_1_task_regex.controller.Regex.*;
+import static homework.block_1_task_regex.view.Messages.*;
 
 public class InputController {
 
@@ -17,15 +20,24 @@ public class InputController {
     private String skype;
 
     public void getInput(UtilityController uController) {
-        surname = uController.inputValueWithScanner(Messages.SURNAME_NAME_MESSAGE, Regex.NAME_NAME_REGEX);
-        name = uController.inputValueWithScanner(Messages.FIRST_NAME_MESSAGE, Regex.NAME_NAME_REGEX);
-        familyName = uController.inputValueWithScanner(Messages.FAMILY_NAME_MESSAGE, Regex.NAME_NAME_REGEX);
-        nickName = uController.inputValueWithScanner(Messages.NICK_NAME_MESSAGE, Regex.NICK_NAME_REGEX);
-        commentary = uController.inputValueWithScanner(Messages.COMMENTARY_MESSAGE, Regex.COMMENTARY_REGEX);
-        homePhoneNumber = uController.inputValueWithScanner(Messages.HOME_PHONE_NUMBER_MESSAGE, Regex.HOME_PHONE_NUMBER_REGEX);
-        mobilePhoneNumber = uController.inputValueWithScanner(Messages.MOBILE_PHONE_NUMBER_MESSAGE, Regex.MOBILE_PHONE_NUMBER_REGEX);
-        email = uController.inputValueWithScanner(Messages.EMAIL_NAME_MESSAGE, Regex.EMAIL_NAME_REGEX);
-        skype = uController.inputValueWithScanner(Messages.SKYPE_NAME_MESSAGE, Regex.SKYPE_NAME_REGEX);
+        String name_surname_familyNameRegex;
+        String nickNameRegex;
+        if (View.bundle.getLocale().equals("en")) {
+            name_surname_familyNameRegex = US_NAME_REGEX;
+            nickNameRegex = US_NICK_NAME_REGEX;
+        } else {
+            name_surname_familyNameRegex = UA_NAME_REGEX;
+            nickNameRegex = UA_NICK_NAME_REGEX;
+        }
+        surname = uController.inputValueWithScanner(SURNAME_NAME_MESSAGE, name_surname_familyNameRegex);
+        name = uController.inputValueWithScanner(FIRST_NAME_MESSAGE, name_surname_familyNameRegex);
+        familyName = uController.inputValueWithScanner(FAMILY_NAME_MESSAGE, name_surname_familyNameRegex);
+        nickName = uController.inputValueWithScanner(NICK_NAME_MESSAGE, nickNameRegex);
+        commentary = uController.inputValueWithScanner(COMMENTARY_MESSAGE, COMMENTARY_REGEX);
+        homePhoneNumber = uController.inputValueWithScanner(HOME_PHONE_NUMBER_MESSAGE, HOME_PHONE_NUMBER_REGEX);
+        mobilePhoneNumber = uController.inputValueWithScanner(MOBILE_PHONE_NUMBER_MESSAGE, MOBILE_PHONE_NUMBER_REGEX);
+        email = uController.inputValueWithScanner(EMAIL_NAME_MESSAGE, EMAIL_NAME_REGEX);
+        skype = uController.inputValueWithScanner(SKYPE_NAME_MESSAGE, SKYPE_NAME_REGEX);
     }
 
     public String getSurname() {
