fc33f7bafda8d0f4ca3bb4b8faecb831e15dc62f
Aleks1696 <aleks.borodavka@ukr.net>
2019-02-17 02:31:50 +0200

    Constants values was changed to ResurceBundels links

--- src/main/java/homework/block_1_task_regex/view/Messages.java
+++ src/main/java/homework/block_1_task_regex/view/Messages.java
@@ -2,17 +2,17 @@ package homework.block_1_task_regex.view;
 
 public interface Messages {
 
-    String INFO_MESSAGE = "Create new note following next hints";
+    String INFO_MESSAGE = "input.start.message";
 
-    String ERROR_MESSAGE = "Wrong input format. Try again";
+    String ERROR_MESSAGE = "input.wrong.format";
 
-    String SURNAME_NAME_MESSAGE = "Enter surname (example: 'Ivanova')";
-    String FIRST_NAME_MESSAGE = "Enter first name (example: 'Maria')";
-    String FAMILY_NAME_MESSAGE = "Enter family name (example: 'Dmitrievna')";
-    String NICK_NAME_MESSAGE = "Enter nick name (example: 'Masha82')";
-    String COMMENTARY_MESSAGE = "Enter commentary (example: 'This note is creating for business purposes ect.')";
-    String HOME_PHONE_NUMBER_MESSAGE = "Enter home phone number (example: '+XXX-XXX-XX-XX')";
-    String MOBILE_PHONE_NUMBER_MESSAGE = "Enter mobile phone number (example: '+XXX(XX)-XXX-XX-XX')";
-    String EMAIL_NAME_MESSAGE = "Enter email (example: 'maria@gmail.com')";
-    String SKYPE_NAME_MESSAGE = "Enter skype (example: 'masha_superStar')";
+    String SURNAME_NAME_MESSAGE = "input.surname";
+    String FIRST_NAME_MESSAGE = "input.firstName";
+    String FAMILY_NAME_MESSAGE = "input.familyName";
+    String NICK_NAME_MESSAGE = "input.nickName";
+    String COMMENTARY_MESSAGE = "input.commentary";
+    String HOME_PHONE_NUMBER_MESSAGE = "input.home.phoneNumber";
+    String MOBILE_PHONE_NUMBER_MESSAGE = "input.mobile.phoneNumber";
+    String EMAIL_NAME_MESSAGE = "input.email";
+    String SKYPE_NAME_MESSAGE = "input.skype";
 }
