a44e71d9baac8e18a6c9ec7380aaf67ff6dc936f
Aleks1696 <aleks.borodavka@ukr.net>
2019-02-17 02:30:41 +0200

    ResourseBundle was added

--- src/main/java/homework/block_1_task_regex/view/View.java
+++ src/main/java/homework/block_1_task_regex/view/View.java
@@ -1,8 +1,18 @@
 package homework.block_1_task_regex.view;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class View {
 
+    static String MESSAGES_BUNDLE_NAME = "messages";
+    public static final ResourceBundle bundle =
+            ResourceBundle.getBundle(
+                    MESSAGES_BUNDLE_NAME,
+                    new Locale("ua"));
+//                    new Locale("en"));
+
     public void print(String message) {
-        System.out.println(message);
+        System.out.println(bundle.getString(message));
     }
 }
