2f6bc96ba668709559955e018ecfec38a22c390a
Aleks1696 <aleks.borodavka@ukr.net>
2019-02-21 22:24:32 +0200

    Note was simplified to only 'nickName' and 'email' fields

--- src/main/java/homework/block_6_task_registrationForm/app/model/Model.java
+++ src/main/java/homework/block_6_task_registrationForm/app/model/Model.java
@@ -1,21 +1,17 @@
 package homework.block_6_task_registrationForm.app.model;
 
-import homework.block_6_task_registrationForm.app.exceptions.WrongInputDataException;
+import homework.block_6_task_registrationForm.app.exceptions.InputDataIsNotUniqueException;
 import homework.block_6_task_registrationForm.app.model.entity.EnumDBNote;
 import homework.block_6_task_registrationForm.app.model.entity.Note;
 
 public class Model {
-
     private Note note;
 
-    public void createNote(String surname, String name, String familyName,
-                           String nickName, String commentary, String homePhoneNumber,
-                           String mobilePhoneNumber, String email, String skype) {
-        note = new Note(surname, name, familyName, nickName, commentary, homePhoneNumber,
-                mobilePhoneNumber, email, skype);
+    public void createNote(String nickName, String email) {
+        note = new Note(nickName, email);
     }
 
-    public void addNoteToDB() throws WrongInputDataException {
+    public void addNoteToDB() throws InputDataIsNotUniqueException {
         EnumDBNote.checkNoteIfExist(note);
     }
 
--- src/main/java/homework/block_6_task_registrationForm/app/model/entity/EnumDBNote.java
+++ src/main/java/homework/block_6_task_registrationForm/app/model/entity/EnumDBNote.java
@@ -1,54 +1,31 @@
 package homework.block_6_task_registrationForm.app.model.entity;
 
-import homework.block_6_task_registrationForm.app.exceptions.WrongInputDataException;
+import homework.block_6_task_registrationForm.app.exceptions.InputDataIsNotUniqueException;
 
-public enum  EnumDBNote {
-    USER_1("Tenslie", "Robert", "Markovich", "robbie7",
-            "Just for fun", "+456-455-45-45", "+077(09)-456-67-67",
-            "rob@gmil.com", "kaon"),
-    USER_2("Roling", "Mat", "Davidson", "MAT",
-            "For business purposes", "+778-324-34-46",
-            "+345(23)-445-87-12","Roling_Mat@unp.com", "mat_778"),
-    USER_3("Прокопенко", "Іван", "Миколайович", "іван",
-            "Створено для ...", "+044-525-12-90",
-            "+380(93)-999-34-34","procopenKO@ukr.net", "Ivan_89");
+public enum EnumDBNote {
+    USER_1("robbie7", "rob@gmil.com"),
+    USER_2("MAT", "Roling_Mat@unp.com"),
+    USER_3("іван", "procopenKO@ukr.net");
 
-    private String surname;
-    private String name;
-    private String familyName;
     private String nickName;
-    private String commentary;
-    private String homePhoneNumber;
-    private String mobilePhoneNumber;
     private String email;
-    private String skype;
 
-    EnumDBNote(String surname, String name, String familyName, String nickName,
-               String commentary, String homePhoneNumber, String mobilePhoneNumber,
-               String email, String skype) {
-        this.surname = surname;
-        this.name = name;
-        this.familyName = familyName;
+    EnumDBNote(String nickName, String email) {
         this.nickName = nickName;
-        this.commentary = commentary;
-        this.homePhoneNumber = homePhoneNumber;
-        this.mobilePhoneNumber = mobilePhoneNumber;
         this.email = email;
-        this.skype = skype;
     }
 
-    public static void checkNoteIfExist(Note note) throws WrongInputDataException {
+    public static void checkNoteIfExist(Note note) throws InputDataIsNotUniqueException {
         String nickName = note.getNickName();
         String email = note.getEmail();
         for (EnumDBNote user : EnumDBNote.values()) {
             if (nickName.equals(user.nickName)) {
-                throw new WrongInputDataException(
+                throw new InputDataIsNotUniqueException(
                         "User with nick name: %s is already registered", nickName);
             } else if (email.equals(user.email)) {
-                throw new WrongInputDataException(
+                throw new InputDataIsNotUniqueException(
                         "User with email: %s is already registered", email);
             }
         }
     }
-
 }
