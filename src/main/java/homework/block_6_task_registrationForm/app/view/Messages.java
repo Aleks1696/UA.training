1ad366f1aa528ae3eb4b5848f8640e3becffd327
Aleks1696 <aleks.borodavka@ukr.net>
2019-02-21 17:47:07 +0200

    View and Message constants

---
+++ src/main/java/homework/block_6_task_registrationForm/app/view/Messages.java
@@ -0,0 +1,18 @@
+package homework.block_6_task_registrationForm.app.view;
+
+public interface Messages {
+
+    String INFO_MESSAGE = "input.start.message";
+
+    String ERROR_MESSAGE = "input.wrong.format";
+
+    String SURNAME_NAME_MESSAGE = "input.surname";
+    String FIRST_NAME_MESSAGE = "input.firstName";
+    String FAMILY_NAME_MESSAGE = "input.familyName";
+    String NICK_NAME_MESSAGE = "input.nickName";
+    String COMMENTARY_MESSAGE = "input.commentary";
+    String HOME_PHONE_NUMBER_MESSAGE = "input.home.phoneNumber";
+    String MOBILE_PHONE_NUMBER_MESSAGE = "input.mobile.phoneNumber";
+    String EMAIL_MESSAGE = "input.email";
+    String SKYPE_MESSAGE = "input.skype";
+}
