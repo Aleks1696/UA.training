8bf3829a92de7bf8415b140145aa0b1a89280f8a
Aleks1696 <aleks.borodavka@ukr.net>
2019-03-15 01:29:31 +0200

    stream used instead of for()

--- src/main/java/homework/block_8_task_collections/Task1_CountElements.java
+++ src/main/java/homework/block_8_task_collections/Task1_CountElements.java
@@ -6,10 +6,16 @@ public class Task1_CountElements {
     public static void main(String[] args) {
         List<Integer> elementsList = new ArrayList<>(Arrays.asList(4, 5, -6, 4, 5, 3, 4, 2, 4, 5, 7));
         Task1_CountElements task = new Task1_CountElements();
-        task.printResult(task.countElementRepeats(elementsList));
+        task.printResult(task.countElementRepeatsCycle(elementsList));
     }
 
-    public <T> Map<T, Integer> countElementRepeats(List<T> elementsList) {
+    private  <T> Map<T, Integer> countElementRepeatsCycle(List<T> elementsList) {
+        Map<T, Integer> resultMap = new LinkedHashMap<>();
+        elementsList.stream().sorted().forEach(x -> resultMap.put(x, resultMap.getOrDefault(x, 0) + 1));
+        return resultMap;
+    }
+
+    private <T> Map<T, Integer> countElementRepeatsStream(List<T> elementsList) {
         Map<T, Integer> resultMap = new Hashtable<>();
         for (T element : elementsList) {
             if (!resultMap.containsKey(element)) {
@@ -22,9 +28,7 @@ public class Task1_CountElements {
         return resultMap;
     }
 
-    public void printResult(Map mapToPrint) {
+    private void printResult(Map mapToPrint) {
         System.out.println(mapToPrint);
     }
-
-
 }
